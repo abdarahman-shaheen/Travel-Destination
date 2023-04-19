9893c9e10cb525cb0eadc8b1d2a223709e0a3419
Abdelrahman shaheen <a.shaheen20001@gmail.com>
2023-04-19 16:07:58 +0000

    name and image

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,18 @@
+
+import './Footer.css'
+
+function Footer(){
+
+    return(
+        <div id="footer">
+        <ul><li>
+            <a href="https://www.facebook.com/profile.php?id=100005316136853">Facebook</a>
+      </li>
+      <li>  Phone: 0791855139</li>
+      <li>  location: Amman</li>
+       </ul>
+        </div>
+    )
+}
+
+export default Footer;
